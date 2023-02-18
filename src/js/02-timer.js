b661029010efb176d8c5c8dd82edf6079c76ec45
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-02-18 13:35:47 +0700

    ex 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,11 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+// all modules
+import Notiflix from 'notiflix';
+
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,65 @@
+// all modules
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const delayInput = document.getElementsByName('delay');
+const stepInput = document.getElementsByName('step');
+const amountInput = document.getElementsByName('amount');
+const formBtn = document.querySelector('button');
+
+formBtn.addEventListener('click', onFormSubmit);
+
+function onFormSubmit(evt){
+  evt.preventDefault();
+  const amount = amountInput[0].value;
+  const delayStep = stepInput[0].value;
+  const delayFirst = delayInput[0].value;
+  let delay = 0;
+  // for (let i = 1; i <= amount; i += 1){
+  //   let position = i;
+   
+    for (let position = 1; position <= Number(amountInput.value); position++) {
+      delay = Number(delayInput.value) + Number(stepInput.value) * (position - 1);
+
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+   Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  });
+  }
+  form.reset();
+}
+
+// formBtn.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   let delay = 0;
+//   for (let position = 1; position <= amountInput; position++) {
+//   delay = delayFirst + delayStep*(position-1);
+//   createPromise (position, delay).then(({ position, delay }) => {
+//     Notify.success(`:white_check_mark: Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notify.failure(`:x: Rejected promise ${position} in ${delay}ms`);
+//   });
+// };
+// })
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((resolve, reject) => {
+    setTimeout (() => {
+      if (shouldResolve){
+        resolve ({ position, delay});
+      }
+      else {
+        reject ({ position, delay});
+      }
+    }, delay);
+  });
+  
 }
+
+
